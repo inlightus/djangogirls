c3c9aff22304c761f9459e08e911e2a09af1e15d
NOR <inlight.ur.life@gmail.com>
2019-01-07 00:40:02 +0000

    implemented auth

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
 }
 
 
+# Added for LOGIN Authenticatioin
+LOGIN_REDIRECT_URL = '/'
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
